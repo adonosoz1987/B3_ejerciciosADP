520999c73d9815281182a30c3f584dfc2e9397bc
adonosoz1987 <arturo.donoso@alumno.iplacex.cl>
2026-08-18 00:52:10 -0400

    feat: agrega proyecto Maven y primeras pruebas automatizadas

---
+++ demo-tests/src/test/java/PrimerTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrimerTest {
+
+    @Test
+    void pruebaSuma() {
+        int resultado = 2 + 3;
+        assertEquals(5, resultado);
+    }
+}
---
+++ demo-tests/src/test/java/SegundoTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SegundoTest {
+
+    @Test
+    void pruebaVerdad() {
+        assertTrue(10 > 1);
+    }
+}
